42e7d748d8fcf05398feb1056514c2685a409b21
Juwon Jeong <2jw0718@gmail.com>
2021-02-24 12:37:38 +0900

    update

--- src/screens/StackNavigatorScreen.js
+++ src/screens/StackNavigatorScreen.js
@@ -1,27 +1,55 @@
 import React from 'react';
 import {enableScreens} from 'react-native-screens';
 import {createNativeStackNavigator} from 'react-native-screens/native-stack';
+import {useRoute} from '@react-navigation/native';
 
 import {SafeAreaView, Text, Button, StyleSheet} from 'react-native';
 
 enableScreens();
 const Stack = createNativeStackNavigator();
 
-const Screen = ({route}) => {
+const Custom = (props) => {
+  const route = useRoute();
+  return <Text style={styles.title}>Custom: {route.params?.data}</Text>;
+};
+
+const Screen = ({route, navigation}) => {
   return (
-    <SafeAreaView>
+    <>
       <Text style={styles.title}>{route.name}</Text>
-      <Button title="Home" />
-      <Button title="Notification" />
-      <Button title="Profile" />
-      <Button title="Settings" />
-    </SafeAreaView>
+      <Text style={styles.title}>{route.params?.data}</Text>
+      <Custom />
+      <Button
+        title="Home"
+        onPress={() => {
+          navigation.navigate('Home', {data: 'one'});
+        }}
+      />
+      <Button
+        title="Notifications"
+        onPress={() => {
+          navigation.navigate('Notifications', {data: 'two'});
+        }}
+      />
+      <Button
+        title="Profile"
+        onPress={() => {
+          navigation.navigate('Profile', {data: 'three'});
+        }}
+      />
+      <Button
+        title="Settings"
+        onPress={() => {
+          navigation.navigate('Settings', {data: 'four'});
+        }}
+      />
+    </>
   );
 };
 
 export default function StackNavigatorScreen({navigation}) {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator initialRouteName="Notifications">
       <Stack.Screen name="Home" component={Screen} />
       <Stack.Screen name="Notifications" component={Screen} />
       <Stack.Screen name="Profile" component={Screen} />
